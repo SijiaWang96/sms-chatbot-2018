b1bf13f75ae5352f9eba1cb3ccd3c1e903c61838
Sijia Wang <sijiaw2@andrew.cmu.edu>
2018-10-04 06:16:05 +0800

    small changes

--- app.rb
+++ app.rb
@@ -81,9 +81,9 @@ def determine_response body
       # array_of_lines = IO.readlines("fact.txt")
       #  message = array_of_lines.sample(1).to_s + " "+ "<h1><p>lol"
       else
-      #media = search_giphy_for body
+      media = search_giphy_for body
       #message = "Guagua is traveling. Sent you a photo from" + get_city.to_s
-      media = search_giphy_for get_city
+      #media = search_giphy_for get_city
       end
 
   #end
